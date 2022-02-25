e770b0581c8c1dc02437827b6d6f15d5e38a4718
Tobiasdanielsen123115 <91379009+Tobiasdanielsen123115@users.noreply.github.com>
2022-02-25 12:33:38 +0100

    Add files via upload

---
+++ BudwegCo2/BudwegCo2/ViewModel/CaliperController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace BudwegCo2.ViewModel
+{
+    public class CaliperController
+    {
+        private CaliperViewModel cali;
+        Calculator calc = new Calculator();
+        public void Co2Calc()
+        {
+            calc.CalcAdd(cali.Amount, cali.SavedCo2);
+        }
+    }
+}
---
+++ BudwegCo2/BudwegCo2/ViewModel/CaliperViewModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BudwegCo2.Model;
+
+namespace BudwegCo2.ViewModel
+{
+    public class CaliperViewModel
+    {
+
+        private Caliper caliper;
+        public int Amount { get; set; }
+
+        public double SavedCo2 { get; set; }
+
+        public double AmountCo2Saved { get; set; }
+
+        public CaliperViewModel(Caliper caliper)
+        {
+            Amount = caliper.Amount;
+            SavedCo2 = caliper.SavedCo2;
+            AmountCo2Saved = caliper.AmountCo2Saved;
+        }
+
+        
+    }
+
+}
+
